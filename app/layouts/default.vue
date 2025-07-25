82cbaf3bb6669cb2dfecd136a7d79d84fd84ea1e
Irving Salazar <ISCIrvingDev@gmail.com>
2025-07-24 18:30:16 -0600

    Add main Layout & Page

---
+++ app/layouts/default.vue
@@ -0,0 +1,7 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div>
+    <p>Some default layout content shared across all pages</p>
+    <slot />
+  </div>
+</template>
---
+++ app/pages/index.vue
@@ -0,0 +1,4 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <h1>Index page</h1>
+</template>
